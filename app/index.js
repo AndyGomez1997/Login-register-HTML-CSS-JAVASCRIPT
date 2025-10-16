c1ded217c1450fdff3a8c01635797c2b4ef377b8
xSNOWBABYx <andersonbernardo1997@gmail.com>
2025-10-16 02:28:39 -0400

    second commit

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+//fix para __dirname
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//server
+const app = express();
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log("servidor corriend en puerto", app.get("port"));
+
+//Fix
+
+app.use(express.static(__dirname + "/public"));
+//routes
+app.get("/", (req, res) => res.sendFile(__dirname + "/pages/login.html"));
+app.get("/register", (req, res) =>
+  res.sendFile(__dirname + "/pages/register.html")
+);
